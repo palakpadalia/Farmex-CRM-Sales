d9237db38f5087ca87609c8ff23bac7b4ff88e9b
palakpadalia <palak@sanskartechnolab.com>
2024-11-14 17:47:41 +0530

    Update Sales Person

---
+++ farmex_crm_sales/py/sales_team.py
@@ -0,0 +1,13 @@
+import frappe
+
+
+@frappe.whitelist()
+def set_sales_person(doc, event):
+    if doc.custom_sales_person:
+
+        doc.sales_team = []
+
+        sales_team = doc.append("sales_team", {})
+        sales_team.sales_person = doc.custom_sales_person
+        sales_team.allocated_percentage = 100
+        sales_team.allocated_amount = doc.total
