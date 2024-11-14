1a2944bd46593466e9f6e0cf28f9414f274b8e79
palakpadalia <palak@sanskartechnolab.com>
2024-11-14 17:24:32 +0530

    Report Button

--- farmex_crm_sales/public/js/sales_order.js
+++ farmex_crm_sales/public/js/sales_order.js
@@ -53,6 +53,16 @@ frappe.ui.form.on('Sales Order', {
                 }
             }
         });
-
     },
+
+    refresh: function (frm) {
+        frm.add_custom_button(__('View Account Receivable'), function () {
+
+            const company = frm.doc.company;
+            const customer = frm.doc.customer;
+
+            const url = `/app/query-report/Account%20Receivable%20Report?company=${company}&customer=${customer}`;
+            window.open(url, "_blank");
+        });
+    }
 });
