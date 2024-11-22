fae9bcc32776ffce4314025f73abd9c578047a20
palakpadalia <palak@sanskartechnolab.com>
2024-11-22 17:25:10 +0530

    Voucher Type Removed

--- farmex_crm_sales/farmex_crm_sales/doctype/voucher_type/test_voucher_type.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Palak P and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestVoucherType(FrappeTestCase):
-	pass
