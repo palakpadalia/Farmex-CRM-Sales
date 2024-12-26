43247ca63a84f7ca6a9f2ba730e894b1f5a0f432
palakpadalia <palak@sanskartechnolab.com>
2024-12-26 11:30:09 +0530

    updtae customer wise item price and pick list print format

--- farmex_crm_sales/farmex_crm_sales/doctype/customer_group_wise_item_price/customer_group_wise_item_price.py
+++ farmex_crm_sales/farmex_crm_sales/doctype/customer_group_wise_item_price/customer_group_wise_item_price.py
@@ -11,6 +11,9 @@ class CustomerGroupWiseItemPrice(Document):
 
 	def on_update(self):
 		item_price_list(self)
+
+	def on_trash(self):
+		delete_item_prices(self)
 		
 
 
@@ -30,16 +33,12 @@ def item_price_list(self):
 		existing_prices_map = {
 			(price["item_code"], price["customer"]): price for price in existing_prices
 		}
-		print(existing_prices_map)
-		print("EXISTS PRICE LIST MAPPING=========================\n\n\n\n")
+		
 		  # Create a set of (item_code, customer) keys from self.item_price
 		current_item_price_keys = {
 			(item.item, customer) for item in self.item_price for customer in customer_list
 		}
-		print(current_item_price_keys)
-
-		print("\n\n\n\n\n")
-
+		
 		# Update and create item price base on the exist to update and not exist to create new
 		for item in self.item_price:
 			for customer in customer_list:
@@ -68,3 +67,19 @@ def item_price_list(self):
 				frappe.delete_doc("Item Price", price["name"])
 		frappe.db.commit()
 
+
+
+
+
+def delete_item_prices(self):
+    # get all customers linked with the current form customer group
+    customer_list = frappe.db.get_all(
+        "Customer", filters={"customer_group": self.customer_group}, pluck="name"
+    )
+
+    # delete item price on trash
+    frappe.db.delete(
+        "Item Price", 
+        {"customer": ["in", customer_list], "selling": 1}
+    )
+    frappe.db.commit()
