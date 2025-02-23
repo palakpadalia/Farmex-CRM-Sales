9aa6101343c16a2b9e7ed367e393ec72cfba2abf
palakpadalia <palakpadalia19@gmail.com>
2025-02-23 16:05:42 +0530

    Code enhanced and UOM Filters item wise

--- farmex_crm_sales/public/js/delivery_note.js
+++ farmex_crm_sales/public/js/delivery_note.js
@@ -1,190 +1,112 @@
 let uom_lists = {};
+
 frappe.ui.form.on('Delivery Note', {
     refresh: function (frm) {
-        if (!frm.is_new()) {
-            frm.set_df_property('custom_sales_person', 'reqd', 1);
-        } else {
-            frm.set_df_property('custom_sales_person', 'reqd', 0);
-        }
+        frm.set_df_property('custom_sales_person', 'reqd', !frm.is_new());
 
-          // Add bulk item button for adding in item table
-          if (frm.doc.docstatus < 1) {
+        if (frm.doc.docstatus < 1) {
             frm.add_custom_button(__('Add Items'), function () {
                 show_grouped_item_dialog(frm);
             });
         }
     },
-    onload: function(frm) {
-        if (frm.doc.docstatus == 0) {
-            // Loop through existing items and regenerate UOM filters
+
+    onload: function (frm) {
+        if (frm.doc.docstatus === 0) {
             frm.doc.items.forEach(row => {
                 if (row.item_code) {
                     fetch_uom_list(frm, row);
                 }
             });
-            frappe.call({
-                method: "farmex_crm_sales.py.item.get_available_stock_items",
-                args: { user: frappe.session.user },
-                callback: function(response) {
-                    if (response.message) {
-                        let item_code_lists = response.message; // Store fetched item codes
-                        console.log(item_code_lists)
-                        frm.fields_dict.items.grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
-                            return {
-                                filters: {
-                                    'name': ['in', item_code_lists],
-                                    'is_sales_item': 1,
-                                    'has_variants': 0
-                                }
-                            };
-                        };
-                    }
-                }
-            });
-            frm.fields_dict.items.grid.get_field('item_code').refresh();
+            
+            fetch_available_stock_items(frm);
         }
-        // Set the get_query function for the 'uom' field on form load
-        frm.fields_dict.items.grid.get_field('uom').get_query = function(doc, cdt, cdn) {
-            // Get the current row
-            let row = locals[cdt][cdn];
 
-            // Check if the row has uom_list data
-            if (uom_lists[cdn]) {
-                return { filters: { 'name': ['in', uom_lists[cdn]] } };
-            } else {
-                // If uom_list data is not available, show all UOMs
-                return { filters: { 'name': ['!=', ''] } };
-            }
+        frm.fields_dict.items.grid.get_field('uom').get_query = function (doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return { filters: { 'name': ['in', uom_lists[row.name] || []] } };
         };
     },
 
-    customer (frm) {
-        frappe.call({
-            method: "farmex_crm_sales.py.item.get_available_stock_items",
-            args: { user: frappe.session.user },
-            callback: function(response) {
-                if (response.message) {
-                    let item_code_lists = response.message; // Store fetched item codes
-                    console.log(item_code_lists)
-                    frm.fields_dict.items.grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
-                        return {
-                            filters: {
-                                'name': ['in', item_code_lists],
-                                'is_sales_item': 1,
-                                'has_variants': 0
-                            }
-                        };
-                    };
-                }
-            }
-        });
+    customer(frm) {
+        fetch_available_stock_items(frm);
         frm.refresh_field('items');
     },
 });
 
 frappe.ui.form.on('Delivery Note Item', {
-    item_code: function(frm, cdt, cdn) {
+    item_code: function (frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-        // fetch_uom_list(frm, row);
-        frappe.db.get_doc('Item', row.item_code).then(docs => {
-            frappe.model.set_value(row.doctype, row.name, 'uom', docs.sales_uom || docs.stock_uom);
-        });
-        frappe.db.get_doc('Item', row.item_code)
-        .then(docs => {
-            let uom_list = [];
-            docs.uoms.forEach(uom => {
-                uom_list.push(uom.uom);
-            });
-            uom_lists[cdn] = uom_list;
-            // Trigger a refresh of the 'uom' field to apply the updated get_query function
-            frm.fields_dict.items.grid.get_field('uom').refresh();
+        
+        frappe.db.get_doc('Item', row.item_code).then(doc => {
+            frappe.model.set_value(cdt, cdn, 'uom', doc.sales_uom || doc.stock_uom);
+            update_uom_list(frm, row, doc.uoms);
         });
     },
 });
 
-
-// Function to fetch UOM list
-function fetch_uom_list(frm, row) {
-    frappe.db.get_doc('Item', row.item_code).then(docs => {
-        let uom_list = [];
-        docs.uoms.forEach(uom => {
-            uom_list.push(uom.uom);
-        });
-        uom_lists[row.name] = uom_list;
-        // Refresh the UOM field
-        frm.fields_dict.items.grid.get_field('uom').refresh();
+function fetch_available_stock_items(frm) {
+    frappe.call({
+        method: "farmex_crm_sales.py.item.get_available_stock_items",
+        args: { user: frappe.session.user },
+        callback: function (response) {
+            if (response.message) {
+                let item_code_lists = response.message;
+                frm.fields_dict.items.grid.get_field('item_code').get_query = function () {
+                    return {
+                        filters: {
+                            'name': ['in', item_code_lists],
+                            'is_sales_item': 1,
+                            'has_variants': 0
+                        }
+                    };
+                };
+            }
+        }
     });
 }
 
+function fetch_uom_list(frm, row) {
+    frappe.db.get_doc('Item', row.item_code).then(doc => {
+        update_uom_list(frm, row, doc.uoms);
+    });
+}
 
+function update_uom_list(frm, row, uoms) {
+    uom_lists[row.name] = uoms.map(u => u.uom);
+    frm.fields_dict.items.grid.get_field('uom').refresh();
+}
 
-// add bulk item add in child table
 function show_grouped_item_dialog(frm) {
-    // dialog for select group wise item
     let dialog = new frappe.ui.Dialog({
         title: 'Add Grouped Items',
         fields: [
             {
-                fieldname: 'item_group',
-                label: 'Item Group',
-                fieldtype: 'Link',
-                options: 'Item Group',
-                reqd: 1,
-                onchange: function () {
-                    const selected_group = dialog.get_value('item_group');
-                    if (selected_group) {
-                        fetch_and_populate_items(dialog, selected_group);
-                    }
-                    cur_dialog.refresh();
-                }
+                fieldname: 'item_group', label: 'Item Group', fieldtype: 'Link', options: 'Item Group', reqd: 1,
+                onchange() { fetch_and_populate_items(dialog, dialog.get_value('item_group')); }
             },
             {
-                fieldname: 'items',
-                label: 'Items',
-                fieldtype: 'Table',
+                fieldname: 'items', label: 'Items', fieldtype: 'Table', cannot_add_rows: true,
                 fields: [
-                    {
-                        fieldname: 'item_code',
-                        label: 'Item Code',
-                        fieldtype: 'Link',
-                        options: 'Item',
-                        in_list_view: 1
-                    },
-                    {
-                        fieldname: 'item_name',
-                        label: 'Item Name',
-                        fieldtype: 'Data',
-                        in_list_view: 1
-                    },
-                    {
-                        fieldname: 'qty',
-                        label: 'Qty',
-                        fieldtype: 'Float',
-                        in_list_view: 1,
-                        default: 1 
-                    }
+                    { fieldname: 'item_code', label: 'Item Code', fieldtype: 'Link', options: 'Item', in_list_view: 1 },
+                    { fieldname: 'item_name', label: 'Item Name', fieldtype: 'Data', in_list_view: 1 },
+                    { fieldname: 'qty', label: 'Qty', fieldtype: 'Float', in_list_view: 1, default: 1 }
                 ],
-                data: [],
-                get_data: () => dialog.fields_dict.items.df.data
+                data: [], get_data: () => dialog.fields_dict.items.df.data
             }
         ],
         primary_action_label: 'Add Items',
-        primary_action: function () {
-            let selected_items = cur_dialog.fields_dict.items.df.data.filter(item => cur_dialog.fields_dict.items.grid.get_selected().includes(item.name));
-            remove_blank_rows(frm); // Remove blank rows from sales order item table
+        primary_action() {
+            let selected_items = dialog.fields_dict.items.df.data.filter(item => dialog.fields_dict.items.grid.get_selected().includes(item.name));
+            remove_blank_rows(frm);
 
             if (selected_items.length > 0) {
                 selected_items.forEach(item => {
                     if (item.item_code && item.qty > 0) {
-                        // check item already exists in sales order item table or not
                         let existing_item = frm.doc.items.find(row => row.item_code === item.item_code);
-
                         if (existing_item) {
-                            // if item already exists then quantity add in existing item
                             existing_item.qty += item.qty;
-                            frm.dirty();
                         } else {
-                            // add rows from selected item 
                             let child_row = frm.add_child('items');
                             frappe.model.set_value(child_row.doctype, child_row.name, 'item_code', item.item_code);
                             frappe.model.set_value(child_row.doctype, child_row.name, 'item_name', item.item_name);
@@ -192,56 +114,35 @@ function show_grouped_item_dialog(frm) {
                         }
                     }
                 });
-                frm.refresh_field('items'); // refresh child table field
+                frm.refresh_field('items');
                 dialog.hide();
             } else {
                 frappe.msgprint(__('Please select items to add.'));
             }
         }
     });
-
     dialog.show();
 }
 
 function fetch_and_populate_items(dialog, item_group) {
-    // clear existing table data when chamge item group
     dialog.fields_dict.items.df.data = [];
     dialog.fields_dict.items.grid.refresh();
 
-    // fetch item base on the item group selection
     frappe.call({
         method: 'frappe.client.get_list',
-        args: {
-            doctype: 'Item',
-            fields: ['item_code', 'item_name'],
-            filters: { item_group: item_group }
-        },
-        callback: function (r) {
-            if (r.message) {
-                // add items in dialogue child table
-                const items_data = r.message.map(item => ({
-                    item_code: item.item_code,
-                    item_name: item.item_name,
-                    qty: 1 // Default quantity
-                }));
-                dialog.fields_dict.items.df.data = items_data;
-                dialog.fields_dict.items.grid.refresh();
-            } else {
-                frappe.msgprint(__('No items found for the selected group.'));
-                dialog.fields_dict.items.df.data = [];
-                dialog.fields_dict.items.grid.refresh();
-            }
+        args: { doctype: 'Item', fields: ['item_code', 'item_name'], filters: { item_group } },
+        callback(r) {
+            dialog.fields_dict.items.df.data = r.message.map(item => ({
+                item_code: item.item_code,
+                item_name: item.item_name,
+                qty: 1
+            }));
+            dialog.fields_dict.items.grid.refresh();
         }
     });
 }
 
-// Remove blank rows
 function remove_blank_rows(frm) {
-    
-    frm.doc.items.forEach((row, index) => {
-        if (!row.item_group && !row.item_code) {
-            frm.doc.items.splice(index, 1);
-        }
-    });
+    frm.doc.items = frm.doc.items.filter(row => row.item_code);
     frm.refresh_field('items');
 }
